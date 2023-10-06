6f71e3d764b3e508345a7229448dfbf9f454e325
DELL 5490 <DELL 5490@10.0.0.128>
2023-10-06 16:20:39 +0530

    First commit

---
+++ project2/src/com/tnsif/Encapsulation/Encapsulation.java
@@ -0,0 +1,13 @@
+package com.tnsif.Encapsulation;
+
+public class Encapsulation {
+ public int serialno;
+ public String name;
+ public int age;
+ 
+ 
+ public void show()
+ {
+	 System.out.println("serialno"+serialno+"\t"+"name"+"t"+"age"+age+"\t");
+ }
+}
---
+++ project2/src/com/tnsif/Encapsulation/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.Encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Encapsulation ob =new Encapsulation();
+        ob.serialno=57;
+        ob.name="haritha";
+        ob.age=22;
+        ob.show();
+	}
+
+}
