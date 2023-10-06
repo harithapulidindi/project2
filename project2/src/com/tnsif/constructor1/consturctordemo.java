267bdac686ac362c30d483be868d9b77f0c19cbf
DELL 5490 <DELL 5490@10.0.0.128>
2023-10-06 16:30:49 +0530

    first commit

---
+++ project2/src/com/tnsif/constructor1/consturctordemo.java
@@ -0,0 +1,22 @@
+package com.tnsif.constructor1;
+
+import java.util.Scanner;
+import java.util.scanner;
+
+public class consturctordemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc=new Scanner(System.in);
+        String name,city;
+        int id;
+        System.out.println("enter id");
+        id=sc.nextInt();
+        sc.nextLine();
+        System.out.println("enter city");
+        city=sc.nextLine();
+        System.out.println("enter name");
+        name=sc.nextLine();
+	}
+
+}
---
+++ project2/src/com/tnsif/programs/constructor.java
@@ -0,0 +1,11 @@
+package com.tnsif.programs;
+
+public class constructor {
+   public String name;
+   public String city;
+   public int id;
+   public void show() {
+	   System.out.println("id"+id+"\t"+"name"+name+"\t"+"city"+city+"\t");
+   }
+   
+}
