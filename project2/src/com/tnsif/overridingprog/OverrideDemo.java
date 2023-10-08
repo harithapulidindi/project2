aaeb7bd285e7a93aaa4c7e831d6888237c893848
DELL 5490 <DELL 5490@DESKTOP-5I20TI2>
2023-10-08 15:00:04 +0530

    first commit

--- project2/src/com/tnsif/overridingprog/OverrideDemo.java
+++ project2/src/com/tnsif/overridingprog/OverrideDemo.java
@@ -4,7 +4,8 @@ public class OverrideDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        sbi ob=new sbi();
+        System.out.println(ob.getRateofInterest());
 	}
 
 }
