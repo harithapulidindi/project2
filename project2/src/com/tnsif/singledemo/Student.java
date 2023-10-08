48af1de53633bd16c2d0751e14a206a64b994c30
DELL 5490 <DELL 5490@DESKTOP-5I20TI2>
2023-10-08 15:16:19 +0530

    first commit

--- project2/src/com/tnsif/singledemo/Student.java
+++ project2/src/com/tnsif/singledemo/Student.java
@@ -1,5 +1,36 @@
 package com.tnsif.singledemo;
 
+import com.tnsif.inheritanceprogram.Citizen;
+
 public class Student extends Citizen{
 
-}
+	public Student() {
+		super();
+		
+	}
+	private int rollno;
+	private String collegename;
+	//@override
+	//public String toString() {
+	      //return "student [rollno=" + rollno + ", collegename=" + collegename + "]";
+       //}
+	public Student(String name,long aadharno,String address,long phno, int rollno, String collegename) {
+		super(name,aadharno,address,phno, phno);
+		//TODO Auto-generated constructor
+		this.rollno=rollno;
+		this.collegename=collegename;
+	}
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno=rollno;
+	}
+	public String getcollegename() {
+		return collegename;
+	}
+	public void setcollegename(String collegename) {
+		this.collegename =collegename;
+	}
+	}
+	
