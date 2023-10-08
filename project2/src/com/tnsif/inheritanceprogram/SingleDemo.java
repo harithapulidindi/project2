171d31c8835504ba9f4c40ba8eb5a37f3fe42aef
DELL 5490 <DELL 5490@DESKTOP-5I20TI2>
2023-10-08 14:55:33 +0530

    first commit

--- project2/src/com/tnsif/inheritanceprogram/SingleDemo.java
+++ project2/src/com/tnsif/inheritanceprogram/SingleDemo.java
@@ -4,7 +4,8 @@ public class SingleDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+         Student ob=new  Student("Haritha",12345,"Illaparru",99556677, 0, null);
+         System.out.println(ob);
 	}
 
 }
