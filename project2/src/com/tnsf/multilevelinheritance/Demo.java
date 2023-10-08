71868c8d56f8138b10b2df7807587208bcf5ecd8
DELL 5490 <DELL 5490@DESKTOP-5I20TI2>
2023-10-08 15:33:28 +0530

    first commit

---
+++ project2/src/com/tnsf/multilevelinheritance/Demo.java
@@ -0,0 +1,12 @@
+package com.tnsf.multilevelinheritance;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         City c=new City("india");
+         
+         System.out.println(c);
+	}
+
+}
