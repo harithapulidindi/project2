47a102ca6ce701cb987fdb1ad45d356ae5e06340
DELL 5490 <DELL 5490@DESKTOP-5I20TI2>
2023-10-08 14:50:49 +0530

    first commit

--- project2/src/com/tnsf/multilevelinheritance/State.java
+++ project2/src/com/tnsf/multilevelinheritance/State.java
@@ -2,4 +2,26 @@ package com.tnsf.multilevelinheritance;
 
 public class State {
 
-}
+   public State(String countryname) {
+	   //TODO Auto-generated method stub
+   }
+	 public static void main(String[] args) {
+		 //TODO Auto-generated method stub
+		 
+	 }
+      private String Statename;
+      
+      //@override
+      //public String toString() {
+      //return "State [Statename=" + Statename + "];
+      //}
+      public String getStatename() {
+    	  return Statename;
+      }
+      public void setStatename(String Statename) {
+    	  this.Statename = Statename;
+      }
+    	  
+      }
+      
+      
