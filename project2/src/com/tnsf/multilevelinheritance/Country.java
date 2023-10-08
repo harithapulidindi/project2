04c69a82987ee5bd6401848ea10d0411dcf46078
DELL 5490 <DELL 5490@DESKTOP-5I20TI2>
2023-10-08 15:57:58 +0530

    first commit

--- project2/src/com/tnsf/multilevelinheritance/Country.java
+++ project2/src/com/tnsf/multilevelinheritance/Country.java
@@ -1,5 +1,26 @@
 package com.tnsf.multilevelinheritance;
 
 public class Country {
+     private String countryname;
+     
+     public void country(String countryname) {
+    	     super();
+    	 this.countryname = countryname;
+    	 
+     }
+     
+     public String getCountryname() {
+    	 return countryname;
+     }
+     
+     public void setCountryname(String countryname) {
+    	 this.countryname = countryname;
+     }
+     
+     @Override
+     public String toString() {
+    	 return "Country[countryname=" + countryname + "]";
+     }
+    	 
+     }
 
-}
