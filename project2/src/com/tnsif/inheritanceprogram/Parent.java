cac9238c965bcf82eb59e9c7e34bcbf34e8ae4cb
DELL 5490 <DELL 5490@DESKTOP-5I20TI2>
2023-10-08 15:21:33 +0530

    first commit

--- project2/src/com/tnsif/inheritanceprogram/Parent.java
+++ project2/src/com/tnsif/inheritanceprogram/Parent.java
@@ -1,5 +1,25 @@
 package com.tnsif.inheritanceprogram;
 
 public class Parent {
-
+              int var=100;
 }
+
+  public class child extends Parent {
+	  int var=50;
+	  void display() {
+            System.out.println("the child value is"+var);
+		    System.out.println("the child value is"+super.var);
+		    
+	  }
+	  
+  public static void main(String[]agrs) {
+	  //TODO Auto-generated method stub
+	  
+	  superdemo ob=new superdemo();
+	  ob.display();
+	  
+  }
+	  
+  }
+	  
+  
