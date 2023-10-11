76da0574e49c4223c17d1276d43c8e1069d2fb9e
DELL 5490 <DELL 5490@DESKTOP-5I20TI2>
2023-10-11 20:05:58 +0530

    first commit

---
+++ project2/src/com/tnsif/staticprograms/Employee.java
@@ -0,0 +1,12 @@
+package com.tnsif.staticprograms;
+//program to demonstrate static variable
+public class Employee {
+    //instance variable
+    private String name;
+    private int id;
+    
+    //declare static variable
+    static String companyName= "Tns";
+}
+
+    
---
+++ project2/src/com/tnsif/staticprograms/EmployeeDemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.staticprograms;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Employee ob=new Employee("haritha",123);
+         System.out.println(ob);
+         Employee companyName;//accessing 
+	}
+
+}
