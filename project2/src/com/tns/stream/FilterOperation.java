5589c320966732d8d94d13929ce558344256c5da
DELL 5490 <DELL 5490@DESKTOP-5I20TI2>
2023-10-29 15:26:45 +0530

    first commit

---
+++ project2/src/com/tns/stream/FilterOperation.java
@@ -0,0 +1,16 @@
+package com.tns.stream;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FilterOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  List<Integer> s1=Arrays.asList(1,1,2,2,3,3,4,5,6,7,8,9);
+  System.out.println("filter the odd numbers");
+  s1.stream().filter(val->val%2==1).forEach(System.out::println);
+  }
+
+}
